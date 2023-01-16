8ae5245b53656f8ad7ff39ef23576509c3f9f1fe
ivanaMesterLightIt <ivana.mester@lightit.io>
2023-01-16 11:05:08 -0300

    update props name

--- components/shared/BaseTextArea.tsx
+++ components/shared/BaseTextArea.tsx
@@ -1,29 +1,24 @@
 import { FC } from "react";
 
-interface Props {
-    label: string;
-    name: string;
-    defaultValue?: string;
-    rows: number;
-    placeholder?: string;
+interface BaseTextAreaProps {
+label: string;
+name: string;
+defaultValue?: string;
+rows: number;
+placeholder?: string;
 }
 
-export const BaseTextArea: FC<Props> = ({ label, name, defaultValue = '', rows, placeholder = '' }) => {
+export const BaseTextArea: FC<BaseTextAreaProps> = ({ label, name, defaultValue = '', rows, placeholder = '' }) => {
     return (
-        <div>
-            <label htmlFor={ name } className="block text-sm font-medium text-gray-700">
-                { label }
-            </label>
-            <div className="mt-1">
-                <textarea
-                rows={ rows }
-                name={ name }
-                id={ name }
+    <div>
+        <label htmlFor={ name } className="block text-sm font-medium text-gray-700">
+            { label }
+        </label>
+        <div className="mt-1">
+            <textarea rows={ rows } name={ name } id={ name }
                 className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                defaultValue={ defaultValue }
-                placeholder={ placeholder }
-                />
-            </div>
+                defaultValue={ defaultValue } placeholder={ placeholder } />
         </div>
+    </div>
     );
-}
+    }
--- components/shared/Select.tsx
+++ components/shared/Select.tsx
@@ -3,62 +3,54 @@ import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 import { FC } from 'react';
 
-interface Props {
-    options:{ id: number; name: string; }[]
+interface SelectProps {
+options:{ id: number; name: string; }[]
 }
-  
-  function classNames(...classes: any) {
-    return classes.filter(Boolean).join(' ')
-  }
 
-export const Select: FC<Props> = ({ options }) => {
+function classNames(...classes: any) {
+return classes.filter(Boolean).join(' ')
+}
+
+export const Select: FC<SelectProps> = ({ options }) => {
 
     const [selected, setSelected] = useState(options[0])
 
     return (
-        <Listbox value={selected} onChange={setSelected}>
-            {({ open }) => (
-                <>
-                <div className="relative mt-1">
-                    <Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm">
+    <Listbox value={selected} onChange={setSelected}>
+        {({ open }) => (
+        <>
+            <div className="relative mt-1">
+                <Listbox.Button
+                    className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm">
                     <span className="block truncate">{selected.name}</span>
                     <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                         <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
                     </span>
-                    </Listbox.Button>
+                </Listbox.Button>
 
-                    <Transition
-                    show={open}
-                    as={Fragment}
-                    leave="transition ease-in duration-100"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                    >
-                    <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                <Transition show={open} as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100"
+                    leaveTo="opacity-0">
+                    <Listbox.Options
+                        className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                         {options.map((option) => (
-                        <Listbox.Option
-                            key={option.id}
-                            className={({ active }) =>
+                        <Listbox.Option key={option.id} className={({ active })=>
                             classNames(
-                                active ? 'text-white bg-blue-600' : 'text-gray-900',
-                                'relative cursor-default select-none py-2 pl-3 pr-9'
+                            active ? 'text-white bg-blue-600' : 'text-gray-900',
+                            'relative cursor-default select-none py-2 pl-3 pr-9'
                             )
                             }
                             value={option}
-                        >
+                            >
                             {({ selected, active }) => (
                             <>
-                                <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
-                                {option.name}
+                                <span className={classNames(selected ? 'font-semibold' : 'font-normal'
+                                    , 'block truncate' )}>
+                                    {option.name}
                                 </span>
 
                                 {selected ? (
-                                <span
-                                    className={classNames(
-                                    active ? 'text-white' : 'text-blue-600',
-                                    'absolute inset-y-0 right-0 flex items-center pr-4'
-                                    )}
-                                >
+                                <span className={classNames( active ? 'text-white' : 'text-blue-600'
+                                    , 'absolute inset-y-0 right-0 flex items-center pr-4' )}>
                                     <CheckIcon className="h-5 w-5" aria-hidden="true" />
                                 </span>
                                 ) : null}
@@ -67,10 +59,10 @@ export const Select: FC<Props> = ({ options }) => {
                         </Listbox.Option>
                         ))}
                     </Listbox.Options>
-                    </Transition>
-                </div>
-                </>
-            )}
-        </Listbox>
+                </Transition>
+            </div>
+        </>
+        )}
+    </Listbox>
     );
-}
+    }
