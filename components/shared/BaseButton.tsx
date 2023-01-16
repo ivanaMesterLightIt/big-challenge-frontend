908719bde8471dfa89cec45546c950a3f1ba505a
ivanaMesterLightIt <ivana.mester@lightit.io>
2023-01-16 11:04:05 -0300

    add extend html attributes

--- components/shared/BaseButton.tsx
+++ components/shared/BaseButton.tsx
@@ -1,23 +1,17 @@
-import { FC } from "react";
+import { ButtonHTMLAttributes, FC } from "react";
 
-interface Props {
-    callback: Function;
-    type: 'button' | 'submit';
-    buttonClass: 'primary' | 'secondary'
+interface BaseButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+callback: Function;
+buttonClass: 'primary' | 'secondary'
 }
 
-export const BaseButton: FC<Props> = ({ callback, type, buttonClass }) => {
+export const BaseButton: FC<BaseButtonProps> = ({ callback, type, buttonClass }) => {
     return (
-        <button
-            type={ type }
-            className={
-                buttonClass === 'primary'
-                ? "inline-flex items-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-                : "inline-flex items-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-            }
-            onClick={ callback() }
-        >
-            Button text
-        </button>
+    <button type={ type } className={ buttonClass==='primary'
+        ? "inline-flex items-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+        : "inline-flex items-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+        } onClick={ ()  => callback() }>
+        Button text
+    </button>
     );
-}
+    }
--- components/shared/BaseInput.tsx
+++ components/shared/BaseInput.tsx
@@ -1,27 +1,22 @@
-import { FC } from "react";
+import { FC, InputHTMLAttributes } from "react";
 
-interface Props {
-    label: string;
-    name: string;
-    type: 'text' | 'number' | 'email';
-    placeholder: string;
+interface BaseInputProps extends InputHTMLAttributes<HTMLInputElement> {
+label: string;
+name: string;
+placeholder: string;
 }
 
-export const BaseInput: FC<Props> = ({ label, type, name, placeholder }) => {
+export const BaseInput: FC<BaseInputProps> = ({ label, type, name, placeholder }) => {
     return (
-        <div>
-            <label htmlFor={ name } className="block text-sm font-medium text-gray-700">
-                { label }
-            </label>
-            <div className="mt-1">
-                <input
-                type={ type }
-                name={ name }
-                id={ name }
+    <div>
+        <label htmlFor={ name } className="block text-sm font-medium text-gray-700">
+            { label }
+        </label>
+        <div className="mt-1">
+            <input type={ type } name={ name } id={ name }
                 className="block w-full rounded-md py-2 px-1 border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
-                placeholder={ placeholder }
-                />
-            </div>
+                placeholder={ placeholder } />
         </div>
+    </div>
     );
-}
+    }
