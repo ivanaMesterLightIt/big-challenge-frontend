47ce794ed65aa6154439c7c75d376b8ff9a35966
ivanaMesterLightIt <ivana.mester@lightit.io>
2023-01-19 09:41:50 -0300

    extend html components props

--- components/shared/BaseButton.tsx
+++ components/shared/BaseButton.tsx
@@ -1,16 +1,15 @@
 import { ButtonHTMLAttributes, FC } from "react";
 
 interface BaseButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-callback: Function;
 buttonClass: 'primary' | 'secondary'
 }
 
-export const BaseButton: FC<BaseButtonProps> = ({ callback, type, buttonClass }) => {
+export const BaseButton: FC<BaseButtonProps> = ({ buttonClass, ...props }) => {
     return (
-    <button type={ type } className={ buttonClass==='primary'
+    <button className={ buttonClass==='primary'
         ? "inline-flex items-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
         : "inline-flex items-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-        } onClick={ ()  => callback() }>
+        } {...props}>
         Button text
     </button>
     );
--- components/shared/BaseInput.tsx
+++ components/shared/BaseInput.tsx
@@ -3,19 +3,18 @@ import { FC, InputHTMLAttributes } from "react";
 interface BaseInputProps extends InputHTMLAttributes<HTMLInputElement> {
 label: string;
 name: string;
-placeholder: string;
 }
 
-export const BaseInput: FC<BaseInputProps> = ({ label, type, name, placeholder }) => {
+export const BaseInput: FC<BaseInputProps> = ({ label, name, ...props }) => {
     return (
     <div>
         <label htmlFor={ name } className="block text-sm font-medium text-gray-700">
             { label }
         </label>
         <div className="mt-1">
-            <input type={ type } name={ name } id={ name }
+            <input name={ name } id={ name }
                 className="block w-full rounded-md py-2 px-1 border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
-                placeholder={ placeholder } />
+                {...props} />
         </div>
     </div>
     );
