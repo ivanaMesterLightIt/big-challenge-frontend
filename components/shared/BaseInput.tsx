a461c88505a825ed529d4556b9307d36e15135af
ivanaMesterLightIt <ivana.mester@lightit.io>
2023-01-31 11:54:23 -0300

    refactor: update baseInput component

--- components/shared/BaseInput.tsx
+++ components/shared/BaseInput.tsx
@@ -7,7 +7,7 @@ interface BaseInputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 export const BaseInput: FC<BaseInputProps> = ({ label, name, ...props }) => {
   return (
-    <div>
+    <div className='w-full px-3'>
       <label htmlFor={name} className="block text-sm font-medium text-gray-700">
         {label}
       </label>
@@ -15,7 +15,7 @@ export const BaseInput: FC<BaseInputProps> = ({ label, name, ...props }) => {
         <input
           name={name}
           id={name}
-          className="block w-full rounded-md py-2 px-1 border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+          className="block w-full rounded-md py-2 px-1 border border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
           {...props}
         />
       </div>
