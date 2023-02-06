f577c31aee36a25f4a7ed3e5c29fa984783af06b
ivanaMesterLightIt <ivana.mester@lightit.io>
2023-02-06 12:23:01 -0300

    refactor: update baseInput to show error message

--- components/shared/BaseInput.tsx
+++ components/shared/BaseInput.tsx
@@ -3,9 +3,11 @@ import { FC, InputHTMLAttributes } from 'react'
 interface BaseInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string
   name: string
+  error?: string | boolean | { message?: string }
+  errorMessage?: string
 }
 
-export const BaseInput: FC<BaseInputProps> = ({ label, name, ...props }) => {
+export const BaseInput: FC<BaseInputProps> = ({ label, name, error = false, errorMessage, ...props }) => {
   return (
     <div className='w-full px-3'>
       <label htmlFor={name} className="block text-sm font-medium text-gray-700">
@@ -18,6 +20,11 @@ export const BaseInput: FC<BaseInputProps> = ({ label, name, ...props }) => {
           className="block w-full rounded-md py-2 px-1 border border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
           {...props}
         />
+      </div>
+      <div className='h-4'>
+        {
+          !!error && <span className='text-red-500 text-xs'>{errorMessage}</span>
+        }
       </div>
     </div>
   )
