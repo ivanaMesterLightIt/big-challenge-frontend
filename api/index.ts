731b386d6d0dcc00a3092d46aa5741ac6c06f91b
ivanaMesterLightIt <ivana.mester@lightit.io>
2023-02-08 15:05:18 -0300

    refactor: make pr fixes and format

--- api/index.ts
+++ api/index.ts
@@ -2,9 +2,9 @@ import axios from 'axios'
 import { headers } from './common'
 
 export const Api = axios.create({
-    baseURL: 'http://localhost:80/api',
-    headers: headers,
-    withCredentials: true
-  });
-  
-export const csrf = async () => { Api.get('http://localhost:80/sanctum/csrf-cookie')}
+  baseURL: 'http://localhost:80/api',
+  headers: headers,
+  withCredentials: true,
+})
+
+export const csrf = () => Api.get('http://localhost:80/sanctum/csrf-cookie')
