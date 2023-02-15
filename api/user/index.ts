608bce614a2cc7fee094546ad30e97da4cb2c46a
ivanaMesterLightIt <ivana.mester@lightit.io>
2023-02-15 12:50:39 -0300

    refactor: update login for new backend

--- api/user/index.ts
+++ api/user/index.ts
@@ -8,7 +8,19 @@ export type NewUser = {
   type: 'doctor' | 'patient'
 }
 
+export type LoginUser = {
+  email: string
+  password: string
+}
+
+const baseURL = 'http://localhost/api'
+
 export const registerUser = async (user: NewUser) => {
-  const response = await axios.post('http://localhost/api/register', user, {headers})
+  const response = await axios.post(`${baseURL}/register`, user, {headers})
+  return response.data
+}
+
+export const loginUser = async (user: LoginUser) => {
+  const response = await axios.post(`${baseURL}/login`, user, {headers})
   return response.data
 }
