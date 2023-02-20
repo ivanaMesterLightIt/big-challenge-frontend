e878bcaab2ed843b35aeea317d9e2a75e7e7d479
ivanaMesterLightIt <ivana.mester@lightit.io>
2023-02-20 09:11:08 -0300

    feat: add new submission

--- api/patient/index.ts
+++ api/patient/index.ts
@@ -1,14 +1,23 @@
 import axios from 'axios'
 import { baseURL, getAuthedHeaders } from '../common'
 
-
 export type PatientInfo = {
-    phone: string
-    weight: string
-    height: string
-    info: string
-  }
-  
+  phone: string
+  weight: string
+  height: string
+  info: string
+}
 
+export type Submission = {
+  title: string
+  symptoms: string
+}
 export const storePatientInfo = (patientInfo: PatientInfo) =>
-  axios.post(`${baseURL}/info`, patientInfo, { headers: getAuthedHeaders() }).then(({ data }) => data)
+  axios
+    .post(`${baseURL}/info`, patientInfo, { headers: getAuthedHeaders() })
+    .then(({ data }) => data)
+
+export const storeSubmission = (submission: Submission) =>
+  axios
+    .post(`${baseURL}/submissions`, submission, { headers: getAuthedHeaders() })
+    .then(({ data }) => data)
--- pages/new-submission.tsx
+++ pages/new-submission.tsx
@@ -1,9 +1,85 @@
+import { z } from 'Zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import toast, { Toaster } from 'react-hot-toast'
 import { MainLayout } from '../components/layouts/mainLayout'
+import { BackButton } from '../components/shared/BackButton'
+import { BaseButton } from '../components/shared/BaseButton'
+import { BaseInput } from '../components/shared/BaseInput'
+import { BaseTextArea } from '../components/shared/BaseTextArea'
+import { storeSubmission } from '../api/patient'
+
+const submissionSchema = z.object({
+  title: z.string().min(1, { message: 'Title is required' }),
+  symptoms: z.string().min(10, { message: 'Symptoms must be at least 10 characters' }),
+})
+
+export type FormValues = z.infer<typeof submissionSchema>
 
 export default function NewSubmissionPage() {
+  const {
+    reset,
+    formState: { errors },
+    register,
+    handleSubmit,
+  } = useForm<FormValues>({ resolver: zodResolver(submissionSchema) })
+
+  const { mutate } = useMutation({
+    mutationFn: storeSubmission,
+    onSuccess: () => {
+      toast.success('Submission successfully send', {
+        position: 'top-right',
+      })
+      reset()
+    },
+    onError: (error: any) => {
+      toast.error(error.response.data.message, {
+        position: 'top-right',
+      })
+    },
+  })
+
+  const onSubmit: SubmitHandler<FormValues> = data => {
+    mutate(data)
+  }
+
   return (
     <MainLayout userType="PATIENT">
-      <h1 className="text-3xl">This is the patient new submission page</h1>
+      <div className="px-3">
+        <BackButton returnTo={'/patient-home'} />
+        <div className="border-b border-gray-300 pb-5">
+          <h1 className="mt-2 text-xl text-gray-900">New Submission</h1>
+        </div>
+      </div>
+      <form className="w-[513px] mt-4" onSubmit={handleSubmit(onSubmit)}>
+        <div className="w-full flex flex-row items-center justify-between">
+          <BaseInput
+            label="Title"
+            type="title"
+            error={errors.title}
+            errorMessage={errors.title?.message}
+            {...register('title')}
+          />
+        </div>
+        <div className="mt-1 w-full flex flex-row items-center justify-between px-3">
+          <BaseTextArea
+            {...register('symptoms')}
+            label="Symptoms"
+            rows={7}
+            error={errors.symptoms}
+            errorMessage={errors.symptoms?.message}
+          />
+        </div>
+        <div className="mt-6 w-1/3 px-3">
+          <BaseButton
+            buttonClass="primary"
+            text="Send submission"
+            type="submit"
+          />
+        </div>
+      </form>
+      <Toaster />
     </MainLayout>
   )
 }
