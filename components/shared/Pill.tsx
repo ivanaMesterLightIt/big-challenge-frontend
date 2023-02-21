058f829a89fc2e6ea9f2729689514afd505c1737
ivanaMesterLightIt <ivana.mester@lightit.io>
2023-02-21 12:57:23 -0300

    refactor: update pill component

--- components/shared/Pill.tsx
+++ components/shared/Pill.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 
 interface PillProps {
-  status: 'pending' | 'in_progress' | 'done'
+  status: 'pending' | 'in_progress' | 'in progress' | 'done'
 }
 
 export const Pill: FC<PillProps> = ({ status }) => {
@@ -10,7 +10,7 @@ export const Pill: FC<PillProps> = ({ status }) => {
       className={
         status === 'done'
           ? 'inline-flex items-center rounded-full bg-gray-100 px-3 py-0.5 text-sm font-medium text-gray-800 capitalize'
-          : status === 'in_progress'
+          : status === 'in_progress' || status === 'in progress'
           ? 'inline-flex items-center rounded-full bg-green-100 px-3 py-0.5 text-sm font-medium text-green-800 capitalize'
           : 'inline-flex items-center rounded-full bg-blue-100 px-3 py-0.5 text-sm font-medium text-blue-800 capitalize'
       }>
